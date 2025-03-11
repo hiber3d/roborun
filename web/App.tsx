0e0ae158e46194bb61552da9595362dfe8f1ba57
Filip Engberg <filip.engberg@hiberworld.com>
2025-03-11 10:39:03 +0100

    Fix Hiber3D Setup and styling

--- web/App.tsx
+++ web/App.tsx
@@ -1,12 +1,8 @@
 import { Hiber3D } from "@hiber3d/web";
-import { moduleFactory } from "../build/moduleFactory";
 import { RoborunMode } from "roborun/RoborunMode";
 
 export const App = () => (
-  <Hiber3D
-    moduleFactory={moduleFactory}
-    config={{ Renderer: { Quality: 3, MSAASampleCount: 1 } }}
-  >
+  <Hiber3D>
     <RoborunMode />
   </Hiber3D>
 );
