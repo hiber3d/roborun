a945177c9a2d01834c16f7030b7a42dbe2de7889
John Segerstedt @ Hiber <105643142+JohnHiber@users.noreply.github.com>
2025-03-12 17:09:08 +0100

    Update OnDeath.js

--- assets/scripts/OnDeath.js
+++ assets/scripts/OnDeath.js
@@ -17,7 +17,7 @@
     }
     
     if (event === "PlayerDied") {
-      hiber3d.print("!!GAME OVER!! - points:'" + Math.round(payload.stats.points) + "' | collectibles:'" + payload.stats.collectibles + "'| meters:'" + Math.round(payload.stats.meters) + "' | multiplier at end: 'x" + (Math.floor(stats.multiplier * 10 + 0.0001) / 10).toFixed(1) + "'");
+      hiber3d.print("!!GAME OVER!! - points:'" + Math.round(payload.stats.points) + "' | collectibles:'" + payload.stats.collectibles + "'| meters:'" + Math.round(payload.stats.meters) + "' | multiplier at end: 'x" + (Math.floor(payload.stats.multiplier * 10 + 0.0001) / 10).toFixed(1) + "'");
       const playerEntity = hiber3d.getValue("GameState", "playerEntity");
       hiber3d.writeEvent("PlayAnimation", { entity: playerEntity, name: "dying", layer: ANIMATION_LAYER.DYING, loop: false });
       hiber3d.writeEvent("PlayAnimation", { entity: playerEntity, name: "dead", layer: ANIMATION_LAYER.DEAD, loop: true });
