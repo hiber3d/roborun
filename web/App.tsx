c951cd61946ab0c9a6431d9d21129336811a8e8f
John Segerstedt <john.segerstedt@hiberworld.com>
2025-03-12 14:55:39 +0100

    [HIB-33651] Lowers graphical quality

--- web/App.tsx
+++ web/App.tsx
@@ -2,7 +2,19 @@ import { Hiber3D } from "@hiber3d/web";
 import { RoborunMode } from "roborun/RoborunMode";
 
 export const App = () => (
-  <Hiber3D>
+  <Hiber3D
+    config={{
+      Renderer: {
+        Quality: 0,
+        MSAASampleCount: 0,
+        EnableNormalMaps: true,
+        MaxVerticalResolution: 1080,
+      },
+      Shadows: {
+        Enabled: false,
+      }
+    }}
+  >
     <RoborunMode />
   </Hiber3D>
 );
