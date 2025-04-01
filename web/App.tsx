13d04489fadce35bafe9428ce876ea5feb3e8007
John Segerstedt <john.segerstedt@hiberworld.com>
2025-04-01 09:36:57 +0200

    [HIB-33938] Removes deprecated configs

--- web/App.tsx
+++ web/App.tsx
@@ -5,15 +5,6 @@ import { RoborunMode } from "roborun/RoborunMode";
 export const App = () => (
   <Hiber3D
     config={{
-      Renderer: {
-        Quality: 0,
-        MSAASampleCount: 0,
-        EnableNormalMaps: true,
-        MaxVerticalResolution: 1080,
-      },
-      Shadows: {
-        Enabled: false,
-      },
       Editor: {
         Enabled: false,
         Mode: "play",
