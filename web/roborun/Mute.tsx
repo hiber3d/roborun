8c5faf927837c11d236730336258587ee981a5fb
perbaltzar <per.baltzar@hiberworld.com>
2025-04-01 09:02:27 +0200

    add mute button

---
+++ web/roborun/Mute.tsx
@@ -0,0 +1,26 @@
+import { useAudio } from "audio/useAudio";
+import { Volume2, VolumeOff } from "lucide-react";
+
+export const Mute = () => {
+  const { music, sfx } = useAudio();
+
+  const isMuted = music.isMuted && sfx.isMuted;
+
+  const toggleMute = () => {
+    const shouldMute = !music.isMuted || !sfx.isMuted;
+    music.mute(shouldMute);
+    sfx.mute(shouldMute);
+  };
+
+  const size = 32;
+
+  return (
+    <div className="pointer-events-auto flex flex-col gap-2 absolute  text-slate-200 top-0 right-0 p-4 ">
+      {isMuted ? (
+        <VolumeOff size={size} onClick={toggleMute} />
+      ) : (
+        <Volume2 size={size} onClick={toggleMute} />
+      )}
+    </div>
+  );
+};
