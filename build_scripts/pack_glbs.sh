992c1b59911095b3133e0355aba6079ef189cad3
Filip Engberg <filip.engberg@hiberworld.com>
2025-04-02 20:59:18 +0200

    gltfpack

---
+++ build_scripts/pack_glbs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Directory that contains glTF files
+INPUT_DIR="assets/input_glbs"
+
+# Output directory
+OUTPUT_DIR="assets/glbs"
+
+# Run find on all *.glb files
+find "$INPUT_DIR" -type f -name "*.glb" | while read -r file; do
+
+    # Construct the output path by trimming off the INPUT_DIR and prefixing OUTPUT_DIR
+    # For example, if file == assets/input_glbs/folderA/scene.glb
+    # then out == assets/glbs/folderA/scene.glb
+    relative="${file#${INPUT_DIR}/}"
+    out="${OUTPUT_DIR}/${relative}"
+
+    # Make sure the output folder exists
+    mkdir -p "$(dirname "$out")"
+
+    # Run gltfpack with desired arguments
+    ./build_scripts/gltfpack -i "$file" -tc -tq 10 -cc -mm -noq -o "$out"
+
+    echo "Converted: $file -> $out"
+done
+
+echo "All .glb files have been processed."
