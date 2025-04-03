1e97cb5c7bfee24099181ff4ecca90ca7cef5a89
perbaltzar <per.baltzar@hiberworld.com>
2025-04-03 14:33:50 +0200

    Magnet UI

---
+++ assets/scripts/ui/Magnet.js
@@ -0,0 +1,18 @@
+({
+    shouldRun() {
+        return hiber3d.getValue("GameState", "alive")
+      },
+    onCreate() {
+        hiber3d.call("rmlCreateDataModel", "magnet_model"); // Getting a Data model already exist
+    },
+    update() {
+        if (!this.shouldRun()) {
+            hiber3d.call("rmlSetDataModelString", "magnet_model", "visible", "0");
+            return;
+        }
+        const playerEntity = hiber3d.getValue("GameState", "playerEntity");
+        const playerHasMagnet = hiber3d.hasScripts(playerEntity, "scripts/powerups/Magnet.js") ? "1" : "0";
+
+        hiber3d.call("rmlSetDataModelString", "magnet_model", "visible", playerHasMagnet);
+    },
+});
