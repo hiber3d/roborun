1f15bb3fcf7137b353a2384d9812b813e0d29389
John Segerstedt <john.segerstedt@hiberworld.com>
2025-04-10 10:16:12 +0200

    [HIB-34006] Bumps RoboRun repo to v1.108.0

--- web/App.tsx
+++ web/App.tsx
@@ -1,9 +1,12 @@
 import { Hiber3D } from "@hiber3d/web";
+import { moduleFactory as moduleFactoryWebGPU } from "GameTemplate_webgpu";
+import { moduleFactory as moduleFactoryWebGL } from "GameTemplate_webgl";
 import { AudioProvider } from "audio/AudioProvider";
 import { RoborunMode } from "roborun/RoborunMode";
 
 export const App = () => (
   <Hiber3D
+    build={{webGPU: moduleFactoryWebGPU as any, webGL: moduleFactoryWebGL as any}}
     logs={{ showLogsInBrowserConsole: true }}
     config={{
       Editor: {
