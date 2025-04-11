8ce8f7509fa2e334fed3106b30165f65ea6283f3
perbaltzar <per.baltzar@hiberworld.com>
2025-04-11 15:49:17 +0200

    .

--- assets/scripts/ui/Magnet.js
+++ assets/scripts/ui/Magnet.js
@@ -1,19 +1,28 @@
 ({
-    shouldRun() {
-        return hiber3d.getValue("GameState", "alive")
-      },
-    onCreate() {
-        hiber3d.call("rmlCreateDataModel", "magnet_model"); // Getting a Data model already exist
-        // hiber3d.call("rmlLoadFont", "fonts/Roboto-Regular.ttf"); // Need to be formatted like this to work
-    },
-    update() {
-        if (!this.shouldRun()) {
-            hiber3d.call("rmlSetDataModelString", "magnet_model", "visible", "0");
-            return;
-        }
-        const playerEntity = hiber3d.getValue("GameState", "playerEntity");
-        const playerHasMagnet = hiber3d.hasScripts(playerEntity, "scripts/powerups/Magnet.js") ? "1" : "0";
+  shouldRun() {
+    return hiber3d.getValue("GameState", "alive");
+  },
+  onCreate() {
+    hiber3d.call("rmlCreateDataModel", "magnet_model"); // Getting a Data model already exist
+  },
+  update() {
+    if (!this.shouldRun()) {
+      hiber3d.call("rmlSetDataModelString", "magnet_model", "visible", "0");
+      return;
+    }
+    const playerEntity = hiber3d.getValue("GameState", "playerEntity");
+    const playerHasMagnet = hiber3d.hasScripts(
+      playerEntity,
+      "scripts/powerups/Magnet.js"
+    )
+      ? "1"
+      : "0";
 
-        hiber3d.call("rmlSetDataModelString", "magnet_model", "visible", playerHasMagnet);
-    },
+    hiber3d.call(
+      "rmlSetDataModelString",
+      "magnet_model",
+      "visible",
+      playerHasMagnet
+    );
+  },
 });
