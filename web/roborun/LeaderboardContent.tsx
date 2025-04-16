9ca5088c438c57495c9e0942386fd85eb71af5a4
John Segerstedt @ Hiber <105643142+JohnHiber@users.noreply.github.com>
2025-04-16 14:48:36 +0200

    [HIB-34134] Adds a 1 sec delay to the leaderboard after death (#105)

--- web/roborun/LeaderboardContent.tsx
+++ web/roborun/LeaderboardContent.tsx
@@ -85,6 +85,7 @@ export const LeaderboardContent = ({
       key="leaderboard"
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
+      transition={{delay:1}}
       className="absolute w-full h-full flex items-center justify-center backdrop-blur-sm"
     >
       <div className="flex flex-col gap-2 max-w-[95%]">
