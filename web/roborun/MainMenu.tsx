76c08e2a210441359b543d48f3ff49dc7b93a47d
Filip Engberg <filip.engberg@hiberworld.com>
2025-04-28 11:44:13 +0200

    wip

--- web/roborun/MainMenu.tsx
+++ web/roborun/MainMenu.tsx
@@ -34,11 +34,11 @@ export const MainMenu = ({ player, fetchRank }: MainMenuProps) => {
   }, [api, canvasRef]);
 
   useEffect(() => {
-    if (!visible || !player?.uuid) {
+    if (!visible) {
       return;
     }
     fetchRank();
-  }, [player?.uuid, visible, fetchRank]);
+  }, [visible, fetchRank]);
 
   const handlePlayButtonClick = () => {
     api?.writeStartInput();
@@ -56,11 +56,18 @@ export const MainMenu = ({ player, fetchRank }: MainMenuProps) => {
 
   return (
     <motion.div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between p-2">
-      <div className="flex justify-between">
-        <div></div>
-        <div>
-          Leaderboard {player?.name} - {player?.rank}
-        </div>
+      <div className="flex justify-between items-start">
+        {player?.rank && (
+          <div className="relative">
+            <img src="ui/menus/ButtonC.webp" className="w-[220px]" />
+            <div className="absolute text-roborun font-roborun text-4xl top-[20px] left-[23px] w-[124px] truncate text-center">
+              {player.name}
+            </div>
+            <div className="absolute text-roborun font-roborun text-3xl bottom-[10px] right-[14px] w-[48px] text-center">
+              #{player.rank}
+            </div>
+          </div>
+        )}
         <Mute />
       </div>
       <div className="flex items-center justify-center">
