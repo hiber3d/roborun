d8cb4fd66561d49917637808be5af03856d9636f
davidhiberbom <david.hesselbom@hiberworld.com>
2025-05-07 10:21:56 +0200

    Fixed crash

--- assets/scripts/audio/KillOnAudioFinished.js
+++ assets/scripts/audio/KillOnAudioFinished.js
@@ -5,6 +5,10 @@
     if (!hiber3d.hasComponents(this.entity, "Hiber3D::AudioComponent")) {
       // TODO: Destroy this.entity entity here!
     }
+    else if (hiber3d.hasComponents(this.entity, "Hiber3D::AudioComponent")) {
+      if (hiber3d.getValue(this.entity, "Hiber3D::AudioComponent", "isFinished"));
+      // TODO: Destroy this.entity entity here!
+    }
   },
   onEvent(event, payload) {
   }
