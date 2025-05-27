799c0d6d8f370be094dbc0b62f86af02e024e748
Per Baltzar <per.baltzar@hiberworld.com>
2025-05-27 09:44:34 +0200

    Post status message (#129)

--- web/utils/postMessage.ts
+++ web/utils/postMessage.ts
@@ -8,10 +8,27 @@ type Payload = {
   payload: Score[];
 };
 
+type StatusPayload = {
+  action: "hiber3d_postStatus";
+  payload: {
+    isNewRound: boolean;
+  };
+};
+
 export const postScores = (scores: Score[]) => {
   const payload: Payload = {
     action: "hiber3d_postScores",
     payload: scores,
+  };
+  window.parent.postMessage(payload, "*");
+};
+
+export const postStatus = () => {
+  const payload: StatusPayload = {
+    action: "hiber3d_postStatus",
+    payload: {
+      isNewRound: true,
+    },
   };
   window.parent.postMessage(payload, "*");
 };
