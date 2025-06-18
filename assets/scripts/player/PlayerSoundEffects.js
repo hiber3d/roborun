46643f89955c36177b32cf61e4f5b85125c66b8c
davidhiberbom <david.hesselbom@hiberworld.com>
2025-06-18 16:30:32 +0200

    Move playAudio function into AudioUtils

--- assets/scripts/player/PlayerSoundEffects.js
+++ assets/scripts/player/PlayerSoundEffects.js
@@ -1,3 +1,5 @@
+import * as audioUtils from "scripts/utils/AudioUtils.js";
+
 export default class {
   shouldRun() {
     return hiber3d.hasComponents(this.entity, "Hiber3D::ComputedWorldTransform");
@@ -12,40 +14,27 @@ export default class {
     hiber3d.addEventListener(this, "BroadcastTurned");
   }
 
-  playSound(parent, name, asset, playSpeed, volume) {
-    const sfxEntity = hiber3d.call("createEntityAsChild", parent);
-    hiber3d.addComponent(sfxEntity, "Hiber3D::AudioSource");
-    hiber3d.addComponent(sfxEntity, "Hiber3D::Name"); // This is just to make it easier to trace which sounds have been added
-
-    hiber3d.setComponent(sfxEntity, "Hiber3D::Name", name);
-    hiber3d.setComponent(sfxEntity, "Hiber3D::AudioSource", "asset", asset);
-    hiber3d.setComponent(sfxEntity, "Hiber3D::AudioSource", "playSpeed", playSpeed);
-    hiber3d.setComponent(sfxEntity, "Hiber3D::AudioSource", "volume", volume);
-
-    hiber3d.setComponent(sfxEntity, "Hiber3D::AudioSource", "playbackMode", 3);
-  }
-
   onEvent(event, payload) {
     if (!this.shouldRun()) {
       return;
     }
     if (event === "JumpedEvent") {
-      this.playSound(this.entity, "Jumped", "audio/sfx/jump_01.mp3", 1, 0.6);
+      audioUtils.playAudio(this.entity, "Jumped", "audio/sfx/jump_01.mp3", 1, 0.6);
     }
     else if (event === "LandedEvent") {
-      this.playSound(this.entity, "Landed", "audio/sfx/land_01.mp3", 1, 0.6);
+      audioUtils.playAudio(this.entity, "Landed", "audio/sfx/land_01.mp3", 1, 0.6);
     }
     else if (event === "BroadcastPowerupPickup") {
-      this.playSound(this.entity, "PowerupPickup", "audio/sfx/rocket_01.mp3", 1, 0.6);
+      audioUtils.playAudio(this.entity, "PowerupPickup", "audio/sfx/rocket_01.mp3", 1, 0.6);
     }
     else if (event === "BroadcastSlided") {
-      this.playSound(this.entity, "Slided", "audio/sfx/roll_01.mp3", 1, 0.9);
+      audioUtils.playAudio(this.entity, "Slided", "audio/sfx/roll_01.mp3", 1, 0.9);
     }
     else if (event === "BroadcastTilted") {
-      this.playSound(this.entity, "Tilted", "audio/sfx/tilt_01.mp3", 0.9 + Math.random() * 0.2, 0.6);
+      audioUtils.playAudio(this.entity, "Tilted", "audio/sfx/tilt_01.mp3", 0.9 + Math.random() * 0.2, 0.6);
     }
     else if (event === "BroadcastTurned") {
-      this.playSound(this.entity, "Turned", "audio/sfx/turn_0" + Math.floor(1 + Math.random() * 2) + ".mp3", 1, 0.2);
+      audioUtils.playAudio(this.entity, "Turned", "audio/sfx/turn_0" + Math.floor(1 + Math.random() * 2) + ".mp3", 1, 0.2);
     }
   }
 }
--- assets/scripts/ui/SoundEffects.js
+++ assets/scripts/ui/SoundEffects.js
@@ -1,3 +1,5 @@
+import * as audioUtils from "scripts/utils/AudioUtils.js";
+
 const COOL_DOWN_TIMER_MS = 1000;
 
 export default class {
@@ -14,26 +16,14 @@ export default class {
     hiber3d.addEventListener(this, "BroadcastPerfectCollectiblePickup");
   }
 
-  playAudio(parent, name, asset, playSpeed, volume) {
-    // TODO: Move this to the C++ source? Makes it harder to change, because you need to recompile...
-    hiber3d.setSingleton("Hiber3D::AudioSettings", "maxActiveVoices", 32);
-
-    const audioEntity = hiber3d.call("createEntityAsChild", parent);
-    hiber3d.addComponent(audioEntity, "Hiber3D::AudioSource");
-    hiber3d.addComponent(audioEntity, "Hiber3D::Name"); // This is just to make it easier to trace which sounds have been added
-
-    hiber3d.setComponent(audioEntity, "Hiber3D::Name", name);
-    hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "asset", asset);
-    hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "playSpeed", playSpeed);
-    hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "volume", volume);
-
-    hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "playbackMode", 3);
-  }
-
   onEvent(event, payload) {
     if (!this.shouldRun()) {
       return;
     }
+
+    // TODO: Move this to the C++ source? Makes it harder to change, because you need to recompile...
+    hiber3d.setSingleton("Hiber3D::AudioSettings", "maxActiveVoices", 32);
+
     if (event === "BroadcastCollectiblePickup") {
       const now = Date.now();
       // if the last pickup was more than 1 second ago, reset the count
@@ -46,17 +36,11 @@ export default class {
 
       this.lastPickup = now;
 
-      this.playAudio(
-        this.entity,
-        "Collectible" + this.pickupsInARow,
-        "audio/sfx/collectible.mp3",
-        0.8 + this.pickupsInARow * 0.04,
-        0.6
-      );
+      audioUtils.playAudio(this.entity, "Collectible" + this.pickupsInARow, "audio/sfx/collectible.mp3", 0.8 + this.pickupsInARow * 0.04, 0.6);
     } else if (event === "BroadcastGameStarted") {
-      this.playAudio(this.entity, "GameStarted", "audio/sfx/tilt_01.mp3", 1, 0.6);
+      audioUtils.playAudio(this.entity, "GameStarted", "audio/sfx/tilt_01.mp3", 1, 0.6);
     } else if (event === "BroadcastPerfectCollectiblePickup") {
-      this.playAudio(this.entity, "PerfectPickup", "audio/sfx/success_01.mp3", 1, 0.7);
+      audioUtils.playAudio(this.entity, "PerfectPickup", "audio/sfx/success_01.mp3", 1, 0.7);
     }
   }
 }
