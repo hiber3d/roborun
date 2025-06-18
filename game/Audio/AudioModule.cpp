c55f8e245169e20f97ab820b35bee44f7b368f06
davidhiberbom <david.hesselbom@hiberworld.com>
2025-06-18 16:44:58 +0200

    Remove dummy field

--- game/Audio/AudioModule.cpp
+++ game/Audio/AudioModule.cpp
@@ -13,7 +13,7 @@
 
 #include <RoboRun/RoboRunTypes.hpp>
 
-struct SyncedMusic { bool dummy; };
+struct SyncedMusic {};
 
 HIBER3D_REFLECT(HIBER3D_TYPE(SyncedMusic));
 
@@ -23,14 +23,14 @@ static void startSyncedMusicWhenLoaded(Hiber3D::Singleton<Hiber3D::AssetServer> 
                                        Hiber3D::View<Hiber3D::AudioSource, const SyncedMusic> audioComponents) {
     bool allLoaded = true;
 
-    for (auto [entity, audio, syncedMusic] : audioComponents.each()) {
+    for (auto [entity, audio] : audioComponents.each()) {
         if (audio->status == Hiber3D::AudioStatus::PAUSED && assetServer->getLoadState(audio->asset.toUntyped()) != Hiber3D::AssetLoadState::LOADED) {
             allLoaded = false;
         }
     }
 
     if (allLoaded) {
-        for (auto [entity, audio, syncedMusic] : audioComponents.each()) {
+        for (auto [entity, audio] : audioComponents.each()) {
             if (audio->status == Hiber3D::AudioStatus::PAUSED) {
                 audio.mut().status = Hiber3D::AudioStatus::PLAYING;
             }
