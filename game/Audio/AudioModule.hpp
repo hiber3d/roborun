2b2172151b4117af6c0704b734f105f35d009250
davidhiberbom <148200841+davidhiberbom@users.noreply.github.com>
2025-06-19 14:46:16 +0200

    Update AudioModule.hpp

--- game/Audio/AudioModule.hpp
+++ game/Audio/AudioModule.hpp
@@ -2,7 +2,6 @@
 
 #include <Hiber3D/Hiber3D.hpp>
 
-// Used for broadcasting specific state to web layer - should not affect simulation
 class AudioModule : public Hiber3D::Module {
 public:
     void onRegister(Hiber3D::InitContext& context) override;
