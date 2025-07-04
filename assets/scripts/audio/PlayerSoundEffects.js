dc39c1244dd8b1121f313cb701b1618ab7ababeb
davidhiberbom <148200841+davidhiberbom@users.noreply.github.com>
2025-07-04 10:47:01 +0200

    3D audio and audio for Grinder and Processor (#145)

--- assets/scripts/audio/PlayerSoundEffects.js
+++ assets/scripts/audio/PlayerSoundEffects.js
@@ -19,22 +19,22 @@ export default class {
       return;
     }
     if (event === "JumpedEvent") {
-      audioUtils.playAudio(this.entity, "Jumped", "audio/sfx/jump_01.mp3", 1, 0.6);
+      audioUtils.playAudio3D(this.entity, "Jumped", "audio/sfx/jump_01.mp3", 1, 0.6);
     }
     else if (event === "LandedEvent") {
-      audioUtils.playAudio(this.entity, "Landed", "audio/sfx/land_01.mp3", 1, 0.6);
+      audioUtils.playAudio3D(this.entity, "Landed", "audio/sfx/land_01.mp3", 1, 0.6);
     }
     else if (event === "BroadcastPowerupPickup") {
-      audioUtils.playAudio(this.entity, "PowerupPickup", "audio/sfx/rocket_01.mp3", 1, 0.6);
+      audioUtils.playAudio3D(this.entity, "PowerupPickup", "audio/sfx/rocket_01.mp3", 1, 0.6);
     }
     else if (event === "BroadcastSlided") {
-      audioUtils.playAudio(this.entity, "Slided", "audio/sfx/roll_01.mp3", 1, 0.9);
+      audioUtils.playAudio3D(this.entity, "Slided", "audio/sfx/roll_01.mp3", 1, 0.9);
     }
     else if (event === "BroadcastTilted") {
-      audioUtils.playAudio(this.entity, "Tilted", "audio/sfx/tilt_01.mp3", 0.9 + Math.random() * 0.2, 0.6);
+      audioUtils.playAudio3D(this.entity, "Tilted", "audio/sfx/tilt_01.mp3", 0.9 + Math.random() * 0.2, 0.6);
     }
     else if (event === "BroadcastTurned") {
-      audioUtils.playAudio(this.entity, "Turned", "audio/sfx/turn_0" + Math.floor(1 + Math.random() * 2) + ".mp3", 1, 0.2);
+      audioUtils.playAudio3D(this.entity, "Turned", "audio/sfx/turn_0" + Math.floor(1 + Math.random() * 2) + ".mp3", 1, 0.2);
     }
   }
 }
