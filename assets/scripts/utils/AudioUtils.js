a3957bbcede2f1b9657c8792bda4907ac5206adc
davidhiberbom <david.hesselbom@hiberworld.com>
2025-07-04 10:08:41 +0200

    Reduce the amount of duplicate code in AudioUtils.js

--- assets/scripts/utils/AudioUtils.js
+++ assets/scripts/utils/AudioUtils.js
@@ -1,11 +1,8 @@
 import * as audio from "hiber3d:audio";
+import * as registry from "hiber3d:registry";
 
-export function playAudio(parent, name, asset, playSpeed, volume) {
-  const audioEntity = hiber3d.call("createEntityAsChild", parent);
+function addAudioSource(audioEntity, asset, playSpeed, volume) {
   hiber3d.addComponent(audioEntity, "Hiber3D::AudioSource");
-  hiber3d.addComponent(audioEntity, "Hiber3D::Name"); // This is just to make it easier to trace which sounds have been added
-
-  hiber3d.setComponent(audioEntity, "Hiber3D::Name", name);
   hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "asset", asset);
   hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "playSpeed", playSpeed);
   hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "volume", volume);
@@ -13,10 +10,8 @@ export function playAudio(parent, name, asset, playSpeed, volume) {
   hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "playbackMode", audio.AudioPlaybackMode.ONCE);
 }
 
-export function playAudio3D(parent, name, asset, playSpeed, volume) {
-  const audioEntity = hiber3d.call("createEntityAsChild", parent);
-
-  hiber3d.addComponent(audioEntity, "Hiber3D::Transform");
+function addSpatialAudio(audioEntity) {
+  hiber3d.addComponent(audioEntity, "Hiber3D::Transform"); // Required for spatial audio to work
   hiber3d.addComponent(audioEntity, "Hiber3D::SpatialAudio");
   hiber3d.setComponent(
     audioEntity,
@@ -24,18 +19,32 @@ export function playAudio3D(parent, name, asset, playSpeed, volume) {
     "volumeAttenuationModel",
     audio.AudioAttenuationModel.INVERSE_DISTANCE
   );
+
   hiber3d.setComponent(audioEntity, "Hiber3D::SpatialAudio", "inaudibleBehavior", audio.InaudibleBehavior.KEEP_PLAYING);
-  hiber3d.setComponent(audioEntity, "Hiber3D::SpatialAudio", "minAttenuationDistance", 2);
+  hiber3d.setComponent(audioEntity, "Hiber3D::SpatialAudio", "minAttenuationDistance", 1);
   hiber3d.setComponent(audioEntity, "Hiber3D::SpatialAudio", "maxAttenuationDistance", 1000);
   hiber3d.setComponent(audioEntity, "Hiber3D::SpatialAudio", "rolloffFactor", 0.1);
+}
 
-  hiber3d.addComponent(audioEntity, "Hiber3D::AudioSource");
+export function playAudio(parent, name, asset, playSpeed, volume) {
+  const audioEntity = registry.isValid(parent) ? hiber3d.call("createEntityAsChild", parent) : registry.create();
+
   hiber3d.addComponent(audioEntity, "Hiber3D::Name"); // This is just to make it easier to trace which sounds have been added
-
   hiber3d.setComponent(audioEntity, "Hiber3D::Name", name);
-  hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "asset", asset);
-  hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "playSpeed", playSpeed);
-  hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "volume", volume);
 
-  hiber3d.setComponent(audioEntity, "Hiber3D::AudioSource", "playbackMode", audio.AudioPlaybackMode.ONCE);
+  addAudioSource(audioEntity, asset, playSpeed, volume);
+
+  return audioEntity;
+}
+
+export function playAudio3D(parent, name, asset, playSpeed, volume) {
+  const audioEntity = registry.isValid(parent) ? hiber3d.call("createEntityAsChild", parent) : registry.create();
+
+  hiber3d.addComponent(audioEntity, "Hiber3D::Name");
+  hiber3d.setComponent(audioEntity, "Hiber3D::Name", name);
+
+  addSpatialAudio(audioEntity);
+  addAudioSource(audioEntity, asset, playSpeed, volume);
+
+  return audioEntity;
 }
